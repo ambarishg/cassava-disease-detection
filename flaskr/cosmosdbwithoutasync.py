e745a6a290944d855cd67729909ad24f1357d3e3
knowledgefoundation <ambarish.ganguly@gmail.com>
2022-10-24 13:15:31 +0530

    all predictions filter query changed

--- flaskr/cosmosdbwithoutasync.py
+++ flaskr/cosmosdbwithoutasync.py
@@ -95,8 +95,7 @@ def get_items(qry):
     # create a container
     container_obj = get_or_create_container(database_obj, container_name)
      
-    querystring = 'SELECT * FROM c' + \
-        ' WHERE c.filename LIKE '' +  qry + '''  
+    querystring = 'SELECT * FROM c WHERE c.filename LIKE %' +  qry + '%'
         
     allitems = []
     for item in container_obj.query_items(
